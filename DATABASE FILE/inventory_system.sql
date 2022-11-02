33513cc10047dc6cf5af88296ee3eec6b4be0416
ganeshwankhade <93642515+ganeshwankhade@users.noreply.github.com>
2022-11-02 11:13:31 +0530

    Update inventory_system.sql

--- DATABASE FILE/inventory_system.sql
+++ DATABASE FILE/inventory_system.sql
@@ -33,19 +33,9 @@ CREATE TABLE IF NOT EXISTS `products` (
 )ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=UTF8;
 
 INSERT INTO `products` (`id`, `name`, `quantity`, `buy_price`, `sale_price`, `categorie_id`, `media_id`, `date`) VALUES
-(1, 'Demo Product', '48', '100.00', '500.00', 1, 0, '2021-04-04 16:45:51'),
-(2, 'Box Varieties', '12000', '55.00', '130.00', 4, 0, '2021-04-04 18:44:52'),
-(3, 'Wheat', '69', '2.00', '5.00', 2, 0, '2021-04-04 18:48:53'),
-(4, 'Timber', '1200', '780.00', '1069.00', 2, 0, '2021-04-04 19:03:23'),
-(5, 'W1848 Oscillating Floor Drill Press', '26', '299.00', '494.00', 5, 0, '2021-04-04 19:11:30'),
-(6, 'Portable Band Saw XBP02Z', '42', '280.00', '415.00', 5, 0, '2021-04-04 19:13:35'),
-(7, 'Life Breakfast Cereal-3 Pk', '107', '3.00', '7.00', 3, 0, '2021-04-04 19:15:38'),
-(8, 'Chicken of the Sea Sardines W', '110', '13.00', '20.00', 3, 0, '2021-04-04 19:17:11'),
-(9, 'Disney Woody - Action Figure', '67', '29.00', '55.00', 3, 0, '2021-04-04 19:19:20'),
-(10, 'Hasbro Marvel Legends Series Toys', '106', '219.00', '322.00', 3, 0, '2021-04-04 19:20:28'),
-(11, 'Packing Chips', '78', '21.00', '31.00', 4, 0, '2021-04-04 19:25:22'),
-(12, 'Classic Desktop Tape Dispenser 38', '160', '5.00', '10.00', 8, 0, '2021-04-04 19:48:01'),
-(13, 'Small Bubble Cushioning Wrap', '199', '8.00', '19.00', 4, 0, '2021-04-04 19:49:00');
+(1, 'Demo Product', '48', '100.00', '500.00', 1, 0, '2022-10-28 16:45:51'),
+(2, 'Electronics', '12000', '55.00', '130.00', 4, 0, '2022-10-28 18:44:52'),
+(3, 'Wheat', '69', '2.00', '5.00', 2, 0, '2022-10-28 18:48:53');
 
 CREATE TABLE IF NOT EXISTS `sales` (
 `id` int(11) unsigned NOT NULL,
@@ -56,14 +46,14 @@ CREATE TABLE IF NOT EXISTS `sales` (
 )ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 INSERT INTO `sales` (`id`, `product_id`, `qty`, `price`, `date`) VALUES
-(1, 1, 2, '1000.00', '2021-04-04'),
-(2, 3, 3, '15.00', '2021-04-04'),
-(3, 10, 6, '1932.00', '2021-04-04'),
-(4, 6, 2, '830.00', '2021-04-04'),
-(5, 12, 5, '50.00', '2021-04-04'),
-(6, 13, 21, '399.00', '2021-04-04'),
-(7, 7, 5, '35.00', '2021-04-04'),
-(8, 9, 2, '110.00', '2021-04-04');
+(1, 1, 2, '1000.00', '2022-10-29'),
+(2, 3, 3, '15.00', '2022-10-29'),
+(3, 10, 6, '1932.00', '2022-10-29'),
+(4, 6, 2, '830.00', '2022-10-29'),
+(5, 12, 5, '50.00', '2022-10-29'),
+(6, 13, 21, '399.00', '2022-10-29'),
+(7, 7, 5, '35.00', '2022-10-29'),
+(8, 9, 2, '110.00', '2022-10-29');
 
 CREATE TABLE IF NOT EXISTS `users` (
 	`id` int(11) unsigned NOT NULL,
@@ -77,11 +67,11 @@ CREATE TABLE IF NOT EXISTS `users` (
 	) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 INSERT INTO `users` (`id`, `name`, `username`, `password`, `user_level`, `image`, `status`, `last_login`) VALUES
-(1, 'Ganesh', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 1, 'no_image.png', 1, '2022-02-11 19:45:52'),
-(2, 'Nayan', 'special', 'ba36b97a41e7faf742ab09bf88405ac04f99599a', 2, 'no_image.png', 1, '2022-02-11 19:53:26'),
-(3, 'abc', 'user', '12dea96fec20593566ab75692c9949596833adc9', 3, 'no_image.png', 1, '2022-02-11 19:54:46'),
-(4, 'user', 'natie', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 3, 'no_image.png', 1, NULL),
-(5, 'Kevin', 'kevin', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 3, 'no_image.png', 1, '2022-02-11 19:54:29');
+(1, 'Ganesh', 'admin', 'ganesh', 1, 'no_image.png', 1, '2022-02-11 19:45:52'),
+(2, 'Nayan', 'special', 'nayan', 2, 'no_image.png', 1, '2022-02-11 19:53:26'),
+(3, 'abc', 'user', 'abc', 3, 'no_image.png', 1, '2022-02-11 19:54:46'),
+(4, 'user', 'natie', 'user', 3, 'no_image.png', 1, NULL),
+(5, 'Kevin', 'kevin', 'kevin', 3, 'no_image.png', 1, '2022-02-11 19:54:29');
 
 CREATE TABLE IF NOT EXISTS `user_groups` (
 	`id` int(11) NOT NULL,
